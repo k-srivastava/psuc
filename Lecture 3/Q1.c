d574c4f58db1df77d62cfaf64c1f6864eed1873f
k-srivastava <srivastavak1223@gmail.com>
2023-04-13 21:14:48 +0530

    Lecture 3 files.

---
+++ Lecture 3/Q1.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void)
+{
+    uint8_t character;
+    
+    printf("Enter a character: ");
+    scanf("%c", &character);
+    
+    printf("The ASCII of %c is %u.\n", character, character);
+  
+    return 0;
+}
---
+++ Lecture 3/Q3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double fahrenheit;
+    
+    printf("Enter the temperature in fahrenheit: ");
+    scanf("%lf", &fahrenheit);
+    
+    double celsius = (fahrenheit - 32) * 5 / 9;
+    
+    printf("The temperature in celsius is %.2lf.\n", celsius);
+  
+    return 0;
+}
